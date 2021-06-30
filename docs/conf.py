cc2e2c6676d2feb942dc673f16aa65a898e9ae49
MvdS <git@vandersluys.nl>
2021-06-30 14:26:37 +0200

    .readthedocs.yml: add requirements.txt, fail_on_warning and system_packages.
    * .readthedocs.yml: add requirements.txt, fail_on_warning and system_packages.
      + add requirements.txt
        - so that RtD installs all dependencies and builds all docs properly;
        - add to repo as well.
      + add fail_on_warning: True to fail if a dep is missing and send an email;
      + add system_packages: True to give access to e.g. numpy on RtD.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'solarenergy'
-copyright = '2020, Marc van der Sluys'
+copyright = '2021, Marc van der Sluys'
 author = 'Marc van der Sluys'
 
 
