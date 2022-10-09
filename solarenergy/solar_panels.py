1d632d1ea20c33dfa82b41e3aa9d18c14594f57c
MvdS <git@vandersluys.nl>
2022-10-09 18:24:02 +0200

    solar_panels: add pv_cell_temperature() and pv_efficiency().
    * solar_panels:
      + add pv_cell_temperature():
        - estimates the PV-cell temperature as a function of ambient temperature, insolation and wind velocity.
      + add pv_efficiency():
        - computes instantaneous PV effiency at a given PV-cell temperature.

--- solarenergy/solar_panels.py
+++ solarenergy/solar_panels.py
@@ -53,6 +53,66 @@ class SolarPanels:
     p_max:    float = 0.0;  """Maximum electrical power of solar panels or inverter (W)"""
     
 
+def pv_cell_temperature(temp_a, glob_insol, v_wind,  temp_c_noct=45, eta_c_noct=0.15, t_coef=-0.0045, glob_insol_noct=800, temp_a_noct=20, v_wind_noct=1, tau_alp=0.9):
+    """Estimate the PV-cell temperature as a function of ambient temperature, insolation and wind velocity.
+
+    Parameters:
+      temp_a (float):           Ambient temperature (°C or K - ensure all temperatures use the same unit).
+      glob_insol (float):       Global projected insolation on PV cell (W/m2).
+      v_wind (float):           Wind velocity (m/s).
+    
+      temp_c_noct (float):      Cell temperature for nominal operating cell temperature (NOCT; °C or K).
+      eta_c_noct (float):       Cell/module efficiency for NOCT (-).
+      t_coef (float):           Temperature coefficient for Pmpp and η_cell (/K).
+      glob_insol_noct (float):  Projected global insolation on the PV module for NOCT (W/m2).
+      temp_a_noct (float):      Ambient temperature for NOCT (°C or K).
+      v_wind_noct (float):      Wind velocity for NOCT (m/s).
+    
+      tau_alp (float):          Tau alpha (τα): the optical transmission-absorption coefficient for the PV module (0-1).
+    
+    Returns:
+      (float):  PV cell temperature (°C or K).
+    
+    Note:
+      - This follows Duffie & Beckman (2013).
+      - NOCT stands for normal-operation cell temperature and concerns the specifications of the PV module for 
+        realistic conditions.  If not available, use STC specs everywhere instead.
+      - Note that all temperatures should be either in °C or K; they should not be mixed.
+    """
+    
+    # Heat loss to the environment, current vs. NOCT, affected by wind:
+    u_l_fac = (5.7+3.8*v_wind_noct)/(5.7+3.8*v_wind)  # U_L,N / U_L
+    
+    temp_c = (
+        temp_a + ( temp_c_noct - temp_a_noct ) * u_l_fac * glob_insol/glob_insol_noct
+        * ( 1 - eta_c_noct/tau_alp * ( 1 - t_coef * temp_c_noct ) )
+    ) / ( 
+        1 + ( temp_c_noct - temp_a_noct ) *  u_l_fac * glob_insol/glob_insol_noct * (t_coef * eta_c_noct)/tau_alp
+    )
+    
+    return temp_c
+
+
+def pv_efficiency(temp_c, eta_c_stc=0.15, t_coef=-0.0045, temp_c_stc=25):
+    """Compute the instantaneous PV effiency for a given cell temperature.
+    
+    We assume that the efficiency varies linearly with temperature.
+    
+    Parameters:
+      temp_c (float):      Temperature of the PV cell (°C or K).
+      eta_c_stc (float):   Efficiency of PV (+inverter if desired) for standard test conditions (STC) (0-1).
+      t_coef (float):      Temperature coefficient for P_mpp and η (/K; <0).
+      temp_c_stc (float):  Temperature of the cells under Standard Test Conditions (°C or K, same as temp_c).
+     
+    Returns:
+      float:           Efficiency of the solar cell.
+    """
+    
+    eta_c = eta_c_stc * (1 + t_coef * (temp_c - temp_c_stc))
+    
+    return eta_c
+
+
 # Test code:
 if __name__ == '__main__':
     # Solar-panel data:
